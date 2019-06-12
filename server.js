86eea2d45a5bf0c88057eeb4c3a0909afbf44382
Dustin Mathis <dustinmath2121@outlook.com>
2019-06-12 12:24:33 -0500

    test

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-var app = require('express')();
+var express = require('express');
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var path = require('path');
 var bodyParser = require ('body-parser');
 
 var PORT = process.env.PORT || 3001;
-
+var app = express();
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 
 
 
 
-// //Serve static assets if in production
-// if(process.env.NODE_ENV === 'production'){
-//     //SET static folder
-//     app.use(app.static('client/build'));
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //SET static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 
 
-//     });
-// }
+    });
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
